e1005d2a4fdca1f36c8fe2cb8ce6bb3f45d30c6e
Amayra <aditya.kumar@calidig.com>
2022-04-06 16:47:47 +0530

    utils

---
+++ src/axios.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import baseURL from './baseURL';
+
+//api, localdedc,stgdedc
+
+const endPoint = 'localdedc';
+
+const axiosWithToken = token => {
+
+	const AuthHeader = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'Authorization': token,
+		"Ocp-Apim-Subscription-Key": "869163acffda4d148b9f490c72b8b13f",
+		"Ocp-Apim-Trace": true
+	};
+
+	return axios.create({
+		baseURL: `${baseURL+endPoint}/`,
+		headers: AuthHeader
+	});
+}
+
+const axiosWithoutToken = () => {
+
+	const AuthHeader = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		"Ocp-Apim-Subscription-Key": "869163acffda4d148b9f490c72b8b13f",
+		"Ocp-Apim-Trace": true
+	};
+
+	return axios.create({
+		baseURL: `${baseURL+endPoint}/`,
+		headers: AuthHeader
+	});
+}
+
+export {
+	axiosWithoutToken,
+	axiosWithToken
+};
---
+++ src/baseURL.js
@@ -0,0 +1,8 @@
+
+//export default 'http://localhost:7071/';
+
+export default 'https://depapim.azure-api.net/';
+
+//export default 'https://stgdedc.azurewebsites.net/';
+
+//export default 'https://localdedc.azurewebsites.net/';
